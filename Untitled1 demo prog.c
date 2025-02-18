6ece882e29c620b87c1c18bbe1de90f839594d24
Hariprasath06-S <hariprasath9641@gmail.com>
2025-02-18 15:20:47 +0530

    Add files via upload

---
+++ Untitled1 demo prog.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+enum week{jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec};
+int main()
+{
+ enum week month;
+ month=jun;
+ printf("%d",month);
+ return 0;
+
+
+
+}
---
+++ Untitled1 demo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+enum year{jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec};
+int main()
+{
+ enum year month;
+ month=nov;
+ printf("%d",month);
+ return 0;
+}
